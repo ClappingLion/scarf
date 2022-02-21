ed9785532a262a7887ea27c391b44fecbc082fb3
ClappingLion <gheemddl@gmail.com>
2022-02-22 07:50:11 +0900

    code splitting
    ..

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -1,19 +1,11 @@
-// import React from 'react';
-// import { render } from 'react-dom';
-// import App from './layouts/App';
-
-// render(<App />, document.querySelector('#app'));
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from '@layouts/App';
 
-ReactDOM.render(
-  <React.StrictMode>
+render(
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
+  </BrowserRouter>,
+  document.querySelector('#root'),
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
--- front/src/layouts/App/index.tsx
+++ front/src/layouts/App/index.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import loadable from '@loadable/component';
+import { Redirect, Route, Switch } from 'react-router-dom';
 
-const App = () => {
-  return (
-    <div>
-      <p>세팅 성공</p>
-    </div>
-  );
-};
+const LogIn = loadable(() => import('@pages/LogIn'));
+const SignUp = loadable(() => import('@pages/SignUp'));
+const Main = loadable(() => import('@pages/Main'));
+
+const App = () => (
+  <Switch>
+    <Route exact path="/">
+      <Redirect to="/main" />
+    </Route>
+    <Route path="/main" component={Main} />
+    <Route path="/login" component={LogIn} />
+    <Route path="/signup" component={SignUp} />
+  </Switch>
+);
 
 export default App;
